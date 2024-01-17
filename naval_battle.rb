b78278553de740cff7c21704798200e707987c23
raf-zig <ziravin@yandex.ru>
2024-01-17 23:49:22 +0500

    commit 2

--- naval_battle.rb
+++ naval_battle.rb
@@ -1,5 +1,6 @@
-letters = %w(A B C D E F G H I J)
-numbers = %w(1 2 3 4 5 6 7 8 9 10)
+
+LETTERS = %w(A B C D E F G H I J)
+NUMBERS = %w(1 2 3 4 5 6 7 8 9 10)
 
 positions = []
 10.times { positions << [] }
@@ -10,13 +11,41 @@ i = 0
   end
   i += 1
 end
-print '  '
-letters.each { |i| print ' ' + i }
-puts
-index = 0
-positions.each do |i|
-  print index < 9 ? numbers[index] + ' ' : numbers[index]
-  i.each { |i| print i }
-  puts 
-  index += 1
+
+  print '  '
+  LETTERS.each { |i| print ' ' + i }
+  puts
+  index = 0
+  positions.each do |i|
+    print index < 9 ? NUMBERS[index] + ' ' : NUMBERS[index]
+    i.each { |i| print i }
+    puts 
+    index += 1
+  end
+
+
+
+
+3.times do
+  a = rand(9)
+  b = rand(1..9)
+   positions[a][b] = ' *' if positions[a][b] != ' *' and positions[a][b - 1] != ' *' 
+   positions[a][b - 1] = ' *' 
 end
+
+4.times do
+  a = rand(9)
+  b = rand(9)
+   positions[a][b] = ' +' if positions[a][b] != ' +' and positions[a][b] != ' *'
+end
+print '  '
+  LETTERS.each { |i| print ' ' + i }
+  puts
+  index = 0
+  positions.each do |i|
+    print index < 9 ? NUMBERS[index] + ' ' : NUMBERS[index]
+    i.each { |i| print i }
+    puts 
+    index += 1
+  end
+ 
