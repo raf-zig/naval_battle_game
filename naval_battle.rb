90b2731fc7952c0f7b7ada6be1c950edd7af7a89
raf-zig <ziravin@yandex.ru>
2024-01-18 15:48:14 +0500

    transfer logic to methods

--- naval_battle.rb
+++ naval_battle.rb
@@ -1,56 +1,53 @@
 LETTERS = %w(A B C D E F G H I J)
 NUMBERS = %w(1 2 3 4 5 6 7 8 9 10)
 
-positions = []
-10.times { positions << [] }
+spots = []
+10.times { spots << [] }
 i = 0
 10.times do
   10.times do
-    positions[i] << ' .'
+    spots[i] << ' .'
   end
   i += 1
 end
-
-print '  '
-LETTERS.each { |i| print ' ' + i }
-puts
-index = 0
-positions.each do |i|
-  print index < 9 ? NUMBERS[index] + ' ' : NUMBERS[index]
-  i.each { |i| print i }
-  puts 
-  index += 1
-end
-
-i = 0
-while i < 3
-  a = rand(9)
-  b = rand(1..9)
-  if positions[a][b] != ' *' and positions[a][b - 1] != ' *' 
-    positions[a][b] = ' *'
-    positions[a][b - 1] = ' *' 
-    i += 1
+ 
+def show_area(spots)
+  print '  '
+  LETTERS.each { |i| print ' ' + i }
+  puts
+  index = 0
+  
+  spots.each do |i|
+    print index < 9 ? NUMBERS[index] + ' ' : NUMBERS[index]
+    i.each { |i| print i }
+    puts 
+    index += 1
   end
 end
 
-c = 0
-while c < 4
-  a = rand(9)
-  b = rand(9)
-    if positions[a][b] != ' +' and positions[a][b] != ' *'
-      positions[a][b] = ' +'
+def place_ships(spots)
+  i = 0
+  while i < 3
+    a = rand(9)
+    b = rand(1..9)
+    if spots[a][b] != ' *' and spots[a][b - 1] != ' *' 
+      spots[a][b] = ' *'
+      spots[a][b - 1] = ' *' 
+      i += 1
+    end
+  end
+
+  c = 0
+  while c < 4
+    a = rand(9)
+    b = rand(9)
+    if spots[a][b] != ' +' and spots[a][b] != ' *'
+      spots[a][b] = ' +'
       c += 1
     end
+  end
 end
 
-print '  '
-LETTERS.each { |i| print ' ' + i }
-puts
-index = 0
-positions.each do |i|
-  print index < 9 ? NUMBERS[index] + ' ' : NUMBERS[index]
-  i.each { |i| print i }
-  puts 
-  index += 1
-end
- 
+show_area(spots)
+place_ships(spots)
+show_area(spots)
